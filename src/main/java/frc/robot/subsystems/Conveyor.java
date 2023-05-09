2cab3a5c1a0db2c40234bd29c309b9808e23180c
Kacper <127464525+kzajchowski@users.noreply.github.com>
2023-05-09 17:05:13 -0500

    tankdrive

---
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Conveyor extends SubsystemBase {
+    
+}
